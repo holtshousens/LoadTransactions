0f48d1a4afb8fbf1ece52a0c397221d88f830353
Sean Holtshousen <holtshousens@gmail.com>
2016-02-22 22:59:37 +0000

    Initial commit

---
+++ CheckFileFormat.py
@@ -0,0 +1,16 @@
+import csv
+
+
+class CheckFileFormat:
+    def __init__(self, filename, separator, fieldcount):
+        self.filename = filename
+        self.separator = separator
+        self.fieldcount = fieldcount
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    main()
